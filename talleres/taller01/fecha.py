2388a9e861f64125194c736e4d63a7c3225d4813
McEwenAle <mcewenalejandro@gmail.com>
2021-01-29 10:32:32 -0500

    Update fecha.py

--- talleres/taller01/fecha.py
+++ talleres/taller01/fecha.py
@@ -1,7 +1,10 @@
  class Fecha():
     
     def __init__(self,dia, mes, anio)
-      
+       self.__dia = dia
+       self.__mes = mes
+       self.__anio = anio
+     
     def dia(self):
       
     def mes(self):
