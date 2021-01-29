1dfd8ffd2f00c2a1c740e57e8eaa99ad1b65d899
Felipe Henao <72840552+fhenaog@users.noreply.github.com>
2021-01-29 10:57:21 -0500

    Update fecha.py

--- talleres/taller01/fecha.py
+++ talleres/taller01/fecha.py
@@ -1,17 +1,30 @@
+  
  class Fecha():
     
-    def __init__(self,dia, mes, anio)
-       self.__dia = dia
-       self.__mes = mes
-       self.__anio = anio
-     
+    def __init__(self,d, m, a)
+        self.__dia = d
+        self.__mes = m
+        self.__anio = a
+        
     def dia(self):
-      
+        return self.__dia
+    
     def mes(self):
-          
+        return self.__mes
+    
     def anio(self):
-          
+        return self.__anio  
+    
     def comparar(self, otra):
-          
+        x = self.__anio*10000 + self.__mes*100 + self.__dia 
+        y = otro.anio()*10000 + otro.mes()*100 + otro.dia() 
+        
+        if(x>y):
+            return 1
+        elif(x<y):
+            return -1
+        else:
+            return 0        
+        
     def __str__(self): 
-          
+        return "Dia: " + self.__dia + "\n" + "Mes: " + self.__mes + "\n" + "Año: " + self.__anio
