5d155b1cb925bf47c8a25a88da79e2b4567e66b0
McEwenAle <mcewenalejandro@gmail.com>
2021-02-04 11:23:05 -0500

    Entrega 1

---
+++ proyecto/codigo/Entrega1.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+import pandas as pd
+import os, sys
+
+ganadoEnfermoCSVs = "./archivosCSV/ganadoEnfermoCSVs"
+ganadoSanoCSVs = "./archivosCSV/ganadoSanoCSVs"
+
+dirGanadoEnfermoCSVs = os.listdir(ganadoEnfermoCSVs)
+
+dirGanadoSanoCSVs = os.listdir(ganadoSanoCSVs)
+
+dataGanadoEnfermoCSVs = [[]] * len(dirGanadoEnfermoCSVs)
+dataGanadoSanoCSVs = [[]] * len(dirGanadoSanoCSVs)
+
+for i in range(len(dirGanadoEnfermoCSVs)):
+    p = ganadoEnfermoCSVs + "/" + dirGanadoEnfermoCSVs[i]
+    dataGanadoEnfermoCSVs[i] = pd.read_csv(p)
+
+for i in range(len(dirGanadoSanoCSVs)):
+    p = ganadoSanoCSVs + "/" + dirGanadoSanoCSVs[i]
+    dataGanadoSanoCSVs[i] = pd.read_csv(p)
+
+print(dataGanadoEnfermoCSVs)
+print("############")
+print(dataGanadoSanoCSVs)
+
+
