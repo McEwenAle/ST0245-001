a09a7d8d52625e8f28545c8414e709cb703d1c74
Felipe Henao Gomez <pipehenaogomez@gmail.com>
2021-02-18 12:25:53 -0500

    Lab Recursion 1

--- laboratorios/lab01/ejercicioEnLinea/Recursion1.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion1.java
@@ -22,19 +22,23 @@ public class Recursion1 {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
     
+    public int sumDigits(int n) {
+        if(n == 0) return 0;
+        return sumDigits(n/10) + n % 10;
+     }
+    
+     public int countX(String str) {
+        if(str.lastIndexOf("x") == -1 ) return 0;
+        return 1 + countX(str.substring(0,str.lastIndexOf("x")));
+      }
+    
+    public int countHi(String str) {
+        if(str.lastIndexOf("hi")==-1) return 0;
+        return 1 + countHi(str.substring(0,str.lastIndexOf("hi")));
+    }
+    
     public int count11(String str) {
-        ...
-    }
-    
-    public String endX(String str) {
-        ...
-    }
-    
-    private String endXAux(String str, int start) {
-        ...
-    }
-    
-    public String changePi(String str) {
-        ...
-    }
+        if(str.lastIndexOf("11")==-1) return 0;
+        return 1 + count11(str.substring(0,str.lastIndexOf("11")));
+      }
 }
