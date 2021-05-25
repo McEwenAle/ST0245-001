4e9bb23a231466712f697ca1822332a7cf21b67f
McEwenAle <mcewenalejandro@gmail.com>
2021-05-25 16:51:44 -0500

    Entrega 3

--- proyecto/codigo/Main.py
+++ proyecto/codigo/Main.py
@@ -2,14 +2,13 @@ from Entrega1 import ImageCompression
 from Entrega2_3 import Image
 
 def main():
-    # img = Image("./archivosCSV/images.csv")
-    # img.losslessCompress()
-    # img.export("./archivosCSV/compressedmages.csv")
-    imageCompression = ImageCompression()
-    imageCompression.importPhotos()
+    img = Image("/Users/alejandromcewen/Documents/Estudios/Semestre2/EstructurasDeDatosYAlgoritmosI/ST0245-001/proyecto/codigo/archivosCSV/ganadoEnfermoCSVs/cow.csv")
+    print(img.losslessCompress())
+    # imageCompression = ImageCompression()
+    # imageCompression.importPhotos()
     # imageCompression.lossyCompress(2)
-    imageCompression.losslessCompress()
+    # imageCompression.losslessCompress()
     # imageCompression.amplify(2)
-    imageCompression.export()
+    # imageCompression.export()
     
 main()
